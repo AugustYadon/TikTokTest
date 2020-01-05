c286afc7e8edb10d3549758c57ad28b47ad8a582
August Yadon <AugustYadon@gmail.com>
2020-01-05 18:49:23 -0500

    Basic Commit for hosting
    Just making a Git so that I can host the project publicly for ByteDance

---
+++ Assets/_Scripts/ControlSoot.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+
+public class ControlSoot : MonoBehaviour
+{
+    public enum SootState {walking,waving,launching} //Enum just to make it more readable.
+    
+    private Animator _animator;
+
+    private SootState _sootState = SootState.walking; //keeps track of what to do in the gameloop
+    private DateTime _startDelay; //Breaks them up to make them look less static
+    private float _walkingCounter; 
+    private float _walkSpeed; //Helps with the same effect as _startDelay
+    private Quaternion _startAngle;
+    private Quaternion _endAngle;
+    private const float WalkTime = 5f;
+    
+    [SerializeField] private float _gravity = 4.5f; //rather than using physics we're just making our own trajectory with a simplified high drag setup
+    private DateTime _launchTime;
+    private float _launchLength = 2f;
+    private Vector3 _downVelocity = Vector3.zero;
+    
+    /// <summary>
+    /// This is where we randomize and initialize variables when a soot sprite instantiated
+    /// Randomization is to add variation to the creatures so that they look less static
+    /// </summary>
+    private void Start()
+    {
+        _startDelay = DateTime.Now.AddSeconds(Random.Range(0f,3f));
+        _walkSpeed = Random.Range(1f, 2.5f); //
+        
+        _animator = GetComponent<Animator>();
+        _animator.SetFloat("walkspeed", _walkSpeed); //match the animation speed with the 3D movement speed
+        
+        _startAngle = transform.parent.localRotation;
+        _endAngle = _startAngle * Quaternion.Euler(0, 0, 90); //this is based on the orientation of the 3D model
+    }
+
+    /// <summary>
+    /// Standard Update() Gameloop logic to do different actions depending on the current state which we define with bools
+    /// </summary>
+    private void Update()
+    {
+        switch(_sootState)
+        {
+            case SootState.launching:
+                transform.parent.position += ((transform.up * 2f) + _downVelocity) * Time.deltaTime;
+                _downVelocity += Vector3.down * _gravity * Time.deltaTime;
+            
+                if(DateTime.Now > _launchTime.AddSeconds(_launchLength))
+                    Destroy(transform.parent.gameObject);
+                break;
+            case SootState.walking:
+                if(DateTime.Now < _startDelay) return;
+                _walkingCounter += (Time.deltaTime * _walkSpeed)/ WalkTime;
+                transform.parent.localRotation = Quaternion.Lerp(_startAngle,_endAngle, _walkingCounter);
+                break;
+            case SootState.waving:
+                //todo add waving animation
+                break;
+        }
+    }
+
+    /// <summary>
+    /// A function that can be called to fling this Soot Sprite. Only used when the user shakes their head.
+    /// </summary>
+    public void Launch()
+    {
+        if (_sootState == SootState.launching) return;
+        
+        _sootState = SootState.launching;
+        transform.parent.SetParent(null); //make sure we don't still have this soot set as a child of the face 
+        _launchTime = DateTime.Now; // we want to delete these objects once they've significantly exited the viewport
+        _animator.SetBool("launch",true);
+    }
+}
---
+++ Assets/_Scripts/HeadShakeExample.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HeadShakeExample : MonoBehaviour
+{
+    [SerializeField] private Transform _headTransform; //used to track head movement and also to place Soots
+    [SerializeField] private GameObject _sootSpritesPrefab;
+    [SerializeField] private GameObject _exclamation;
+    [SerializeField] private float _speedThreshold;
+    [SerializeField] private float _angularSpeedThreshold;
+    private float[] _recentSpeeds = new float[5];
+    private float[] _recentAngularSpeeds = new float[5];
+    private float _currentAverageSpeed;
+    private float _currentAverageAngularSpeed;
+    private Vector3 _lastPosition;
+    private Quaternion _lastRotation;
+    private int _counter;
+    private bool _isMoving, _isShaking = true; //we start with a shake and wait for the user to be still
+
+    [SerializeField] private bool _useExclamation = false;
+    
+    /// <summary>
+    /// Classic Unity initialization
+    /// </summary>
+    private void Start()
+    {
+        _lastPosition = _headTransform.position;
+        _lastRotation = _headTransform.rotation;
+        _speedThreshold *= _speedThreshold; //Get the square so that we can compare it against square magnitudes
+        StartCoroutine(Shake());
+    }
+
+    /// <summary>
+    /// Game loop mostly for updating the average movement of user's head over the last 5 frames
+    /// Secondly to trigger events, like calling Shake()
+    /// </summary>
+    private void Update()
+    {
+        _currentAverageAngularSpeed -= _recentAngularSpeeds[_counter] / _recentAngularSpeeds.Length;
+        _currentAverageSpeed -= _recentSpeeds[_counter]/_recentSpeeds.Length;
+        
+        _recentAngularSpeeds[_counter] = Quaternion.Angle(_headTransform.rotation, _lastRotation) / Time.deltaTime;
+        _recentSpeeds[_counter] = (_headTransform.position - _lastPosition).sqrMagnitude/Time.deltaTime;
+        
+        _currentAverageAngularSpeed += _recentAngularSpeeds[_counter] / _recentAngularSpeeds.Length;
+        _currentAverageSpeed += _recentSpeeds[_counter]/_recentSpeeds.Length;
+        
+        _counter = ++_counter % _recentSpeeds.Length;
+        
+        _lastPosition = _headTransform.position;
+        _lastRotation = _headTransform.rotation;
+
+        if (_currentAverageSpeed < _speedThreshold && _currentAverageAngularSpeed < _angularSpeedThreshold)
+        {
+            _isMoving = false;
+            return;
+        }
+        if(!_isShaking) //check if head went from moving to stopped during this frame
+        {
+            _isShaking = true;
+            StartCoroutine(Shake());
+        }
+        _isMoving = true;
+    }
+    
+    /// <summary>
+    /// Here we call the Launch() method of each SootSprite to make them fly off your head when you shake it
+    /// We then wait for 2 seconds (And then we also wait until the user is not shaking) until we spawn new Soot Sprites 
+    /// </summary>
+    private IEnumerator Shake()
+    {
+        _headTransform.BroadcastMessage("Launch",SendMessageOptions.DontRequireReceiver);
+        if(_useExclamation)
+            _exclamation.SetActive(true);
+        
+        yield return new WaitForSeconds(2f);
+        _exclamation.SetActive(false);
+        yield return new WaitWhile(() => _isMoving);
+        Instantiate(_sootSpritesPrefab,_headTransform,false);
+        _isShaking = false;
+    }
+    
+}
